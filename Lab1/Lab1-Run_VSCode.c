f16f93d1988192e14dd92fafdf2b0d3c405f7bcd
unknown <flucktv25@gmail.com>
2025-07-11 14:09:45 +0700

    Update

--- Lab1/Lab1-Run_VSCode.c
+++ Lab1/Lab1-Run_VSCode.c
@@ -1,24 +1,3 @@
-## โจทย์
-จงแก้ไขโค้ดต่อไปนี้ ให้สามารถรับค่าจากผู้ใช้ เพื่อแสดงผลบนหน้าจอคอมพิวเตอร์ให้ได้ พร้อมทั้งจัดรูปแบบให้ตรงตาม Syntax ที่เรียนมาในห้องเรียน
-
-```c++
-#include <stduio.h>
-
-int main() {
-    char Name[50] ;
-    int  Age = 0 ;
-    printf( "Enter your name: " ) 
-    scanf( "%s", Name ) ;
-    printf( "Enter your age: " ) ;
-    scanf( "%d", Age ) ;
-    print( "- - - - - -\n" ) ;
-    printf( "Hello %s \n", ___ ) ; 
-    printf( "Age = %d\n", ___ ) ; 
-    
-}//end main function
-```
-
-
 #include <stdio.h>
 
 int main() {
